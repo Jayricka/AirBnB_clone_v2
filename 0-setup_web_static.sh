37c72824221d0b98bd4d0ba360fd33490acdad85
jayricka <jayricka17@gmail.com>
2023-12-07 08:44:36 +0300

    configuration of server

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+# This script sets up web servers for the deployment of web_static.
+
+if ! command -v nginx &> /dev/null; then
+    sudo apt-get update
+    sudo apt-get install -y nginx
+fi
+
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
+
+echo -e "<html>\n\t<head>\n\t</head>\n\t<body>\n\t\tHolberton School\n\t</body>\n</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+sudo chown -R ubuntu:ubuntu /data/
+
+nginx_config="/etc/nginx/sites-available/default"
+sudo tee "$nginx_config" > /dev/null <<EOF
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+
+    root /var/www/html;
+    index index.html;
+
+    location / {
+        try_files \$uri \$uri/ =404;
+    }
+
+    location /hbnb_static {
+        alias /data/web_static/current/;
+    }
+
+    error_page 404 /404.html;
+    location = /404.html {
+        internal;
+    }
+}
+EOF
+
+sudo systemctl restart nginx
+
+exit 0
