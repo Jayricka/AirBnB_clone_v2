fcdced34398eac8da6e8e114b2bc29548ca1bff9
jayricka <jayricka17@gmail.com>
2023-12-12 19:45:39 +0300

    deploying static files

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,19 +1,57 @@
 #!/usr/bin/env bash
+# This script sets up web servers for the deployment of web_static.
 
+# Install Nginx if not already installed
 if ! command -v nginx &> /dev/null; then
-    sudo apt-get update
-    sudo apt-get install -y nginx
+    apt-get update
+    apt-get install -y nginx
 fi
 
-sudo mkdir -p /data/web_static/releases/test /data/web_static/shared
-echo "<html><head></head><body>Holberton School</body></html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
-sudo chown -R ubuntu:ubuntu /data/
+# Create necessary folders
+mkdir -p /data/web_static/releases/test
+mkdir -p /data/web_static/shared
 
-nginx_config="/etc/nginx/sites-available/default"
-if [ -e "$nginx_config" ]; then
-    sudo sed -i '/location \/ {/a \location /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' "$nginx_config"
-fi
+# Create fake HTML file
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" > /data/web_static/releases/test/index.html
 
-sudo service nginx restart
+# Create or recreate symbolic link
+rm -rf /data/web_static/current
+ln -s /data/web_static/releases/test /data/web_static/current
+
+# Give ownership of /data/ folder to ubuntu user and group
+chown -R ubuntu:ubuntu /data/
+
+# Update Nginx configuration
+config_text="
+server {
+    listen 80;
+    server_name _;
+
+    location /hbnb_static {
+        alias /data/web_static/current/;
+        index index.html;
+    }
+
+    location /redirect_me {
+        rewrite ^/redirect_me https://www.youtube.com permanent;
+    }
+
+    error_page 404 /404.html;
+    location = /404.html {
+        root /usr/share/nginx/html;
+        internal;
+    }
+}
+"
+echo "$config_text" > /etc/nginx/sites-available/default
+
+# Restart Nginx
+service nginx restart
+
 exit 0
